e6811672f89a9ee8f61d0ff1259b4ad87eac0617
Arun Varma <arun.verma87@gmail.com>
2016-11-23 22:26:27 +0100

    removing version from application.yml file

--- src/main/java/com/goeuro/service/BusRouteServiceApplication.java
+++ src/main/java/com/goeuro/service/BusRouteServiceApplication.java
@@ -1,12 +1,12 @@
 package com.goeuro.service;
 
 import com.goeuro.core.BusRouteDataContainer;
-import com.goeuro.service.inner.BusRouteServiceConfiguration;
 
 import io.dropwizard.Application;
+import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
 
-public class BusRouteServiceApplication extends Application<BusRouteServiceConfiguration> {
+public class BusRouteServiceApplication extends Application<Configuration> {
 
 	public static void main(String[] args) throws Exception {
 		BusRouteDataContainer.getInstance().createDataStructureFromFile(args[2]);
@@ -15,7 +15,7 @@ public class BusRouteServiceApplication extends Application<BusRouteServiceConfi
 	}
 
 	@Override
-	public void run(BusRouteServiceConfiguration arg0, Environment arg1) throws Exception {
+	public void run(Configuration arg0, Environment arg1) throws Exception {
 		final BusRouteResource resource = new BusRouteResource();
 		arg1.jersey().register(resource);
 	}
